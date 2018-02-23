8ed18229aa77baedd7de50c6b61843fde703485a
Brielle Harrison <nyteshade@gmail.com>
2018-02-22 23:32:04 -0800

    Refactored, extended and cleaned up
    **Changes**
     * Added `gql` as alias to dedent for use with babel syntax highlighting
     * Made `dedent` be `customDedent({dropLowest:true})` as this is
       universally desired
     * Deprecated `customDedent`
     * Added `measureIndents` and the idea behind making it plug and play
       with various functors for pre, per and post modification of values
     * Added no-op functors
     * Converted drop empty first and last line and drop lowest logic to
       functors compatible with `measureIndents`
     * Added comphrehensive unit tests

--- src/functions.js
+++ src/functions.js
@@ -1,5 +1,16 @@
 // @flow
 
+// These constants can be discarded in the next major version where
+// customDedent is no longer present
+const DEDENT = 1
+const DEDENT_DROP_LOWEST = 2
+const DEDENT_FULL = DEDENT | DEDENT_DROP_LOWEST
+
+// A short term solution to manage potential configurations for customDedent
+// in a low logic manner; i.e. without the need for excessive if statements
+// kill this in the next major revision
+const variations: Map<Number, Function> = new Map()
+
 /**
  * One annoyance of working with tag functions is the conversion and inter-
  * weaving of the substitutions to get a string to modify in the first place.
@@ -39,6 +50,208 @@ export function handleSubstitutions(
 }
 
 /**
+ * Nearly identical to `cautiouslyApply`, this function works on an array of
+ * items rather than a single item. The only other difference is that the
+ * supplied functors receive the index of the item and the array the item is
+ * contained within as second and third parameters, respectively.
+ *
+ * @param {Array<mixed>} array an array of values to apply each functor to
+ * @param {Array<Function>} fns an array of `Function` objects that are to be
+ * executed with the supplied `item` as its input and the new value as its
+ * output. An error or fals
+ * @param {boolean} log true if any errors caused by function execution should
+ * be logged
+ * @param {boolean} keepOldValueOnFalseyReturn if true and the functor returns
+ * a falsey value, the value passed to it as a parameter is used instead
+ * @return {Array<mixed>} a copy of the array passed as `array` but with
+ * potentially modified internal values
+ */
+export function cautiouslyApplyEach(
+  array: Array<mixed>,
+  fns: Array<Function>,
+  log: boolean = true,
+  keepOldValueOnFalseyReturn: boolean = false
+): Array<mixed> {
+  let items = Array.from(array)
+
+  if (items && Array.isArray(items) && fns && Array.isArray(fns)) {
+    items.forEach((item, itemIndex, itemArray) => {
+      let newItem = item
+
+      fns.forEach(fn => {
+        try {
+          let bindFunctor = Array.isArray(fn) && fn[1]
+          let functor = bindFunctor
+            ? fn[0].bind(newItem)
+            : Array.isArray(fn) ? fn[0] : fn
+
+          if (keepOldValueOnFalseyReturn)
+            newItem = functor(newItem, itemIndex, itemArray) || newItem
+          else
+            newItem = functor(newItem, itemIndex, itemArray)
+        }
+        catch (error) {
+          if (log) {
+            console.error(error)
+          }
+        }
+      })
+
+      items[itemIndex] = newItem
+    })
+  }
+
+  return items
+}
+
+/**
+ * Given an item that needs to be modified or replaced, the function takes
+ * an array of functions to run in order, each receiving the last state of the
+ * item. If an exception occurs during function execution the value passed is
+ * the value that is kept.
+ *
+ * Optionally, when the function executes and returns the newly modified state
+ * of the object in question, if that value is falsey it can be replaced with
+ * the original value passed to the function instead. This is not true by
+ * default
+ *
+ * `fns` is an array of functions, but any of the functions in the list are
+ * actually a tuple matching [Function, boolean] **and** the boolean value is
+ * true then the function within will be bound to the item it is operating on
+ * as the `this` context for its execution before processing the value. It will
+ * still receive the item as the first parameter as well.
+ *
+ * **NOTE: this will not work on big arrow functions**
+ *
+ * @param {mixed} item any JavaScript value
+ * @param {Array<Function>} fns an array of `Function` objects that are to be
+ * executed with the supplied `item` as its input and the new value as its
+ * output. An error or fals
+ * @param {boolean} log true if any errors caused by function execution should
+ * be logged
+ * @param {boolean} keepOldValueOnFalseyReturn if true and the functor returns
+ * a falsey value, the value passed to it as a parameter is used instead
+ * @return {mixed} the new value to replace the one supplied as `item`
+ */
+export function cautiouslyApply(
+  item: mixed,
+  fns: Array<Function|[Function, boolean]>,
+  log: boolean = true,
+  keepOldValueOnFalseyReturn: boolean = false
+): mixed {
+  if (fns && Array.isArray(fns)) {
+    fns.forEach(fn => {
+      try {
+        let bindFunctor = Array.isArray(fn) && fn[1]
+        let functor = bindFunctor
+          ? fn[0].bind(item)
+          : Array.isArray(fn) ? fn[0] : fn
+
+        if (keepOldValueOnFalseyReturn)
+          item = functor(item) || item
+        else
+          item = functor(item)
+      }
+      catch (error) {
+        if (log) {
+          console.error(error)
+        }
+      }
+    })
+  }
+
+  return item
+}
+
+/**
+ * Measure indents is something that may be of use for any tag function. Its
+ * purpose is to take a string, split it into separate lines and count the
+ * leading whitespace of each line. Once finished, it returns an array of
+ * two items; the list of strings and the matching list of indents which are
+ * related to each other via index.
+ *
+ * The function also receives a config object which allows you to specify up
+ * to three lists worth of functions.
+ *
+ * `preWork` is a list of functions that receive the following arguments in
+ * each callback and are meant as a pluggable way to modify the initial string
+ * programmatically by the consuming developer. Examples might be to prune
+ * empty initial and final lines if they contain only whitespace
+ *
+ * ```
+ * preWorkFn(string: string): string
+ *
+ *   - string: the supplied string to be modified by the function with the
+ *     new version to be used as the returned value. If null or undefined is
+ *     returned instead, the value supplied to the function will be used
+ * ```
+ *
+ * `perLine` is list of functions that receive the following arguments in
+ * each callback and are meant as a pluggable way to modify the line in
+ * question. The expected return value is the newly modified string from which
+ * to measure the indent of. Each item in the list will receive the modified
+ * string returned by its predecessor. Effectively this is a map function
+ *
+ * ```
+ * perLine(string: string, index: number, array: Array<string>): string
+ * ```
+ *
+ * `postWork` is a list of functions that get called in order and receive
+ * the final results in the form of an array containing two elements. The
+ * first is the list of strings and the second is the list of measuredIndents.
+ * The format of the supplied value (i.e. array of two arrays) is expected as
+ * a return value. Failure to do so will likely end up as a bug someplace
+ *
+ * ```
+ * postWork(
+ *   array: Array<Array<string>, Array<number>>
+ * ): Array<Array<string>, Array<number>>
+ * ```
+ *
+ * All functions supplied to these arrays, if wrapped in an array with a
+ * second parameter of true, will cause the function in question to be bound
+ * with the item it is working on as the `this` context.
+ *
+ * @param {string} string see above
+ * @param {Object} config see above
+ * @param {RegExp} whitespace the defintion for whitespaced used within
+ * @return {Array<Array<string>, Array<number>>} an array containing two
+ * arrays; the first being an array of one line per index and the second being
+ * an index matched array of numbered offsets indicating the amount of white
+ * space that line is prefixed with
+ */
+export function measureIndents(
+  string: string|Array<string>,
+  config: Object = {
+    preWork: [],
+    perLine: [],
+    postWork: []
+  },
+  whitespace: RegExp = /[ \t]/
+): Array<Array<string>, Array<number>> {
+  let regexp = new RegExp(`(^${whitespace.source}*)`)
+  let strings
+  let indents
+
+  if (Array.isArray(string)) {
+    string = string.join('\n')
+  }
+
+  string = cautiouslyApply(string, config.preWork, true, true)
+  strings = string.split(/\r?\n/)
+  indents = strings.map((s, i, a) => {
+    let search
+
+    s = cautiouslyApply(s, config.perLine, true, false)
+
+    search = regexp.exec(s);
+    return search && search[1].length || 0;
+  });
+
+  return cautiouslyApply([strings, indents], config.postWork)
+}
+
+/**
  * The underpinning of `dedent()` is `customDedent()`. This function takes
  * an options object and returns a tag function. So direct usage would be
  * something like
@@ -53,57 +266,49 @@ export function handleSubstitutions(
  * lowest values to be equal to the second lowest values.
  *
  * @param {Object} options
+ *
+ * @deprecated the options for customDedent seem to always be preferrable in
+ * practice and as such, `dedent` and `gql` are simply aliases for the base
+ * behavior in `customDedent` now. Expect `customDedent` to be removed in
+ * future major revisions of `ne-tag-fns`
  */
-export function customDedent(
-  options: Object = {
-    dropLowest: undefined
+export function customDedent(options: Object = { dropLowest: true }) {
+  let mask = DEDENT
+    | (options.dropLowest ? DEDENT_DROP_LOWEST : 0)
+
+  if (variations.has(mask)) {
+    return variations.get(mask)
   }
-) {
-  return function (strings, ...subs) {
-    // handle the substitution stuff off the bat, just gets in the way
-    // if we try to handle it inline below
-    strings = handleSubstitutions(strings, subs).split('\n');
 
-    // construct a small resuable function for trimming all initial whitespace
-    let trimL = s => s.replace(/^([ \t]*)/, '');
-    let trimR = s => s.replace(/([ \t]*)($)/, '$1');
+  return function (ss, ...subs) {
+    let [strings, indents] = measureIndents(
+      // handle the substitutions right off the bat, it will just make things
+      // easier all around
+      handleSubstitutions(ss, subs),
 
-    // the first line is usually a misnomer, discount it if it is only whitespace
-    if (!trimL(strings[0]).length) {
-      strings.splice(0, 1);
-    }
-
-    // the same goes for the last line
-    if (!trimL(strings[strings.length - 1]).length) {
-      strings.splice(strings.length - 1, 1);
-    }
-
-    // count the indentation for each line; used below
-    let indents = strings.map(s => {
-      let search = /(^[ \t]*)/.exec(s);
-      return search && search[1].length || 0;
-    });
-
-    // check to see if we should drop lowest (based on lowest being < half)
-    if (options.dropLowest) {
-      let isFn = obj => typeof obj === 'function';
-      let lowest = Math.min(...indents);
-      let occurences = indents.filter(o => o === lowest).length;
-      let nextLowest = Math.min(...[].concat(indents).filter(o => o !== lowest));
-      let test = isFn(options.dropLowest) && options.dropLowest || ((array, lowest, count) => count < array.length / 2);
-
-      if (test(indents, lowest, occurences)) {
-        indents = indents.map(o => o === lowest ? nextLowest : o);
+      // pass in the handlers to strip empty head and tail strings and then
+      // drop the lowest value as this is often a requested feature
+      {
+        preWork: [stripEmptyFirstAndLast],
+        postWork: [
+          options.dropLowest
+            ? dropLowestIndents
+            : NOOP_POSTWORK
+        ]
       }
-    }
+    )
 
     // count the minimal amount of shared leading whitespace
     let excess = Math.min(...indents) || 0;
 
     // if the excessive whitespace is greater than 0, remove the specified
     // amount from each line
-    strings = strings.map(s => s.replace(/([ \t]*)$/, ''));
-    strings = strings.map(s => s.replace(new RegExp(`^[ \t]{0,${excess}}`), ''));
+    if (excess > 0) {
+      strings = strings.map(s => s.replace(/([ \t]*)$/, ''));
+      strings = strings.map(s => s.replace(
+        new RegExp(`^[ \t]{0,${excess}}`), ''
+      ));
+    }
 
     // return a single joined string
     return strings.join('\n');
@@ -125,7 +330,126 @@ export function customDedent(
  * @return {string} a combined and dedented string
  */
 export function dedent(strings, ...subs) {
-  return customDedent()(strings, ...subs)
+  return customDedent({dropLowest: true})(
+    strings,
+    ...subs
+  )
 }
+
+/**
+ * An alternate name for `dedentDropLowest` that can be used to trigger syntax
+ * highlighting when used with GraphQL SDL/IDL strings in some IDEs and editors
+ *
+ * @type {Function}
+ * @see #dedentDropLowest
+ * @see #customDedent
+ */
+export const gql = dedent
+
+// Create cached variation with false for all options
+variations.set(DEDENT,
+  customDedent({dropLowest: false})
+)
+
+// Create cached variation with true for dropping lowest
+variations.set(DEDENT | DEDENT_DROP_LOWEST,
+  customDedent({dropLowest: true})
+)
+
+/**
+ * A `preWork` functor for use with `measureIndents` that strips the first and
+ * last lines from a given string if that string has nothing but whitespace. A
+ * commonly desired functionality when working with multiline template strings
+ *
+ * @param  {string} string the string to parse
+ * @return {string} a modified string missing bits that make up the first and/
+ * or last lines **if** either of these lines are comprised of only whitespace
+ */
+export function stripEmptyFirstAndLast(string: string): string {
+  let strings = string.split(/\r?\n/)
+  // construct a small resuable function for trimming all initial whitespace
+  let trimL = s => s.replace(/^([ \t]*)/, '');
+  let trimR = s => s.replace(/([ \t]*)($)/, '$1');
+
+  // the first line is usually a misnomer, discount it if it is only
+  // whitespace
+  if (!trimL(strings[0]).length) {
+    strings.splice(0, 1);
+  }
+
+  // the same goes for the last line
+  if (!trimL(strings[strings.length - 1]).length) {
+    strings.splice(strings.length - 1, 1);
+  }
+
+  return strings.join('\n')
+}
+
+/**
+ * A `postWork` functor for use with `measureIndents` that will modify the
+ * indents array to be an array missing its lowest number.
+ *
+ * @param {[Array<string>, Array<number>]} values the tuple containing the
+ * modified strings and indent values
+ * @return {[Array<string>, Array<number>]} returns a tuple containing an
+ * unmodified set of strings and a modified indents array missing the lowest
+ * number in the list
+ */
+export function dropLowestIndents(
+  values: [Array<string>, Array<number>]
+): [Array<string>, Array<number>] {
+  let [strings, indents] = values
+  let set = new Set(indents)
+  let lowest = Math.min(...indents)
+
+  set.delete(lowest)
+
+  return [strings, Array.from(set)]
+}
+
+/**
+ * A simple non operation preWork function that can make writing and reading
+ * code that uses measureIndents easier to work with.
+ *
+ * @param {string} string any string value
+ * @return {string} the value passsed in as `string`
+ */
+export function NOOP_PREWORK(string: string): string {
+  return string
+}
+
+/**
+ * A simple non operation preWork function that can make writing and reading
+ * code that uses measureIndents easier to work with.
+ *
+ * @param {string} string any string value
+ * @param {number} index the index of `string` within `array`
+ * @param {Array<string>} array the array containing `string`
+ * @return {string} the value passsed in as `string`
+ */
+export function NOOP_PERLINE(
+  string: string,
+  index: number,
+  array: Array<string>
+): string {
+  return item
+}
+
+/**
+ * A simple non operation postWork function that can make writing and reading
+ * code that uses measureIndents easier to work with.
+ *
+ * @param {[Array<string>, Array<number>]} values the tuple containing the
+ * modified strings and indent values
+ * @return {[Array<string>, Array<number>]} the same value passed in as `values`
+ */
+export function NOOP_POSTWORK(
+  values: [Array<string>, Array<number>]
+): [Array<string>, Array<number>] {
+  return values
+}
+
+// Export the variations in the case someone needs to tweak the runtime
+export { variations }
 
 export default dedent;
--- test/functions.test.js
+++ test/functions.test.js
@@ -1,22 +1,32 @@
-import { dedent } from '../src/functions'
+import {
+  dedent,
+  gql,
+  cautiouslyApply,
+  cautiouslyApplyEach,
+  measureIndents,
+  dropLowestIndents,
+  stripEmptyFirstAndLast
+} from '../src/functions'
 
 describe('dedent', () => {
-  let include = "stdio.h"
-  let pattern = "%d %s"
+  const include = "stdio.h"
+  const pattern = "%d %s"
+
+  // Do not reformat this weird indentation. It is on purpose.
 
             let expression = dedent`
               This is a test of the emergency broadcast system
-              
+
               #include <${include}>
-              
+
               int main(int argc, char **argv) {
                 for (int i = 0; i < argc; i++) {
                   printf("${pattern}", i, argv[i])
                 }
-              }  
+              }
             `
-            
-  let comparison = [
+
+  const comparison = [
     'This is a test of the emergency broadcast system\n',
     '\n',
     '#include <', include, '>\n',
@@ -25,10 +35,119 @@ describe('dedent', () => {
     '  for (int i = 0; i < argc; i++) {\n',
     '    printf("', pattern, '", i, argv[i])\n',
     '  }\n',
-    '}'  
+    '}'
   ].join('')
-  
+
   it('a "dedent"\'ed string should match a handcrafted one', () => {
     expect(expression).toEqual(comparison)
+  })
+})
+
+describe('ensure that aliases are kept up', () => {
+  it('asserts that gql and dedent are the same', () => {
+    expect(gql).toBe(dedent)
+  })
+})
+
+describe('ensure that cautiouslyApply works', () => {
+  it('should be able to modify a number', () => {
+    expect(cautiouslyApply(5, [i => i * 2])).toBe(10)
+  })
+
+  it('should be able to chain effects', () => {
+    expect(cautiouslyApply(5, [i => i * 2, i => i + 2])).toBe(12)
+  })
+
+  it('should ignore errored functions with no change', () => {
+    let answer = cautiouslyApply(5, [
+      i => i * 2,
+      i => { throw new Error() },
+    ], false)
+
+    expect(answer).toBe(10)
+  })
+
+  it('should process things after an error', () => {
+    let answer = cautiouslyApply(5, [
+      i => i * 2,
+      i => { throw new Error() },
+      i => i + 2
+    ], false)
+
+    expect(answer).toBe(12)
+  })
+})
+
+describe('ensure that cautiouslyApplyEach works', () => {
+  let strings = ['he', 'him', 'his']
+  let functors = [
+    i => i.replace(/\bhe\b/, 'she'),
+    i => i.replace(/\bhim\b/, 'her'),
+    i => i.replace(/\bhis\b/, 'hers')
+  ]
+
+  it('should apply each functor to each item', () => {
+    expect(cautiouslyApplyEach(strings, functors)).toEqual(
+      expect.arrayContaining(['she', 'her', 'hers'])
+    )
+  })
+
+  it('should continue to apply even if there is an error', () => {
+    let functorsWError = Array.from(functors)
+
+    functorsWError.splice(1, 0, i => { throw new Error() })
+    expect(cautiouslyApplyEach(strings, functors, false)).toEqual(
+      expect.arrayContaining(['she', 'her', 'hers'])
+    )
+  })
+})
+
+describe('check that measureIndents does what we expect', () => {
+  it('should see simple counts irrespective of line endings', () => {
+    let sample = 'zero\n one\r\n  two\n   three'
+
+    expect(measureIndents(sample)).toEqual(
+      expect.arrayContaining([
+        expect.arrayContaining([
+          'zero',
+          ' one',
+          '  two',
+          '   three'
+        ]),
+        expect.arrayContaining([0, 1, 2, 3])
+      ])
+    )
+  })
+
+  it('should also squash empty first and last with functor', () => {
+    let sample = '    \nzero\n one\r\n  two\n   three\n'
+
+    expect(
+      measureIndents(
+        sample,
+        {
+          preWork: [stripEmptyFirstAndLast]
+        }
+      )
+    ).toEqual(
+      expect.arrayContaining([
+        expect.arrayContaining([
+          'zero',
+          ' one',
+          '  two',
+          '   three'
+        ]),
+        expect.arrayContaining([0, 1, 2, 3])
+      ])
+    )
+  })
+})
+
+describe('check to see that dropLowestIndents works as expected', () => {
+  let values = [2, 4, 4, 6]
+
+  it('should only have 4 as its minimum value', () => {
+    let [, indents] = dropLowestIndents([[], values])
+    expect(Math.min(...indents)).toBe(4)
   })
 })
